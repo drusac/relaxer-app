79e117b7497f280c13e3745960be9d89e6d83eb4
Dominik <rusac.dominik@gmail.com>
2020-09-27 16:37:43 +0200

    breath animation with js trigger

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-const form = document.getElementById('form');
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breathAnimation();
+
+function breathAnimation() {
+    text.innerText = 'Breate In!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'Hold!';
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!';
+        container.className = 'container shrink';
+        }, holdTime);
+    }, breatheTime);
+}
+
+setInterval(breathAnimation, totalTime);
